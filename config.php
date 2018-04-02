2a047de360b277aeb271e24562aef701cd723112
avenirs05 <avenirs05@gmail.com>
2018-04-02 17:05:28 +0300

    last

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://okoshko.ref/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://okoshko.ref/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/Users/Lenovo_note/Documents/OpenServer/domains/okoshko.ref/catalog/');
+define('DIR_SYSTEM', 'C:/Users/Lenovo_note/Documents/OpenServer/domains/okoshko.ref/system/');
+define('DIR_IMAGE', 'C:/Users/Lenovo_note/Documents/OpenServer/domains/okoshko.ref/image/');
+define('DIR_LANGUAGE', 'C:/Users/Lenovo_note/Documents/OpenServer/domains/okoshko.ref/catalog/language/');
+define('DIR_TEMPLATE', 'C:/Users/Lenovo_note/Documents/OpenServer/domains/okoshko.ref/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/Users/Lenovo_note/Documents/OpenServer/domains/okoshko.ref/system/config/');
+define('DIR_CACHE', 'C:/Users/Lenovo_note/Documents/OpenServer/domains/okoshko.ref/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/Users/Lenovo_note/Documents/OpenServer/domains/okoshko.ref/system/storage/download/');
+define('DIR_LOGS', 'C:/Users/Lenovo_note/Documents/OpenServer/domains/okoshko.ref/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/Users/Lenovo_note/Documents/OpenServer/domains/okoshko.ref/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/Users/Lenovo_note/Documents/OpenServer/domains/okoshko.ref/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'okoshko');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
